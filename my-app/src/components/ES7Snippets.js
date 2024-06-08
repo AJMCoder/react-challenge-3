476970361116309769ccf5dd7fa060b27db3f349
Al Mullins <alexmullins@hotmail.co.uk>
2024-06-08 17:20:19 +0000

    Add new login bar and refactor code so that code is created in nested commonent in NavBarChild.js.

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,53 @@
+// import React from 'react'
+
+// rfce* This is the shortcutcode for creating a functional component with an export statement.
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rafce* This is the shortcutcode for creating an arrow function with an export statement.
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// rce* This is the shortcutcode for creating a class component with an export statement.
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// rconst* This is the shortcutcode for creating a constructor with a state.
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+// clg* This is the shortcutcode for console.log()
+// console.log(first)
+
+
+// clo* This is the shortcutcode for console.log() with object
+// console.log('first', first)
